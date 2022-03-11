9930425a574864848b648eadc0e6bbaec8700d8b
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-11 22:04:31 +0530

    comments added

--- 1.basicplotting.py
+++ 1.basicplotting.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+xpoints = np.array([0, 6])   # x poiints of the line
+ypoints = np.array([0, 250]) # y points of the line
 
-plt.plot(xpoints, ypoints)
-plt.show()
+plt.plot(xpoints, ypoints)  # variables should be the same as  assigned as earlier
+plt.show()   # this displayes our graph
+
+
+#Any number of numbers  can be given inside the x and y points.They must be equal in nos.

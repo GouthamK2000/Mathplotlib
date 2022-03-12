6cc2d07fbdbb931147b47ef0698a1b7a1052edf7
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-12 07:29:48 +0530

    all marjkers added

--- 2.plotting_with_o.py
+++ 2.plotting_with_o.py
@@ -1,9 +1,38 @@
-#here only the points are shown
+
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.array([6,7,7,0])
-y=np.array([3,5,1,2])
-plt.plot(x,y,'o') #use o inbetween '' , 'o' to print only the points/nodes
+x=np.array([3,2,1])
+y=np.array([1,2,3])
+plt.plot(x,y, 'o')
 plt.show()
+
+
+#any marker shown below can be  used
+
+# Marker	Description
+# 'o'	    Circle	
+# '*'	    Star	
+# '.'	    Point	
+# ','	   Pixel	
+# 'x'   	X	
+# 'X'  	X (filled)	
+# '+' 	Plus	
+# 'P'	   Plus (filled)	
+# 's'	   Square	
+# 'D'	   Diamond	
+# 'd'	   Diamond (thin)	
+# 'p'	   Pentagon	
+# 'H' 	Hexagon	
+# 'h'	   Hexagon	
+# 'v'	  Triangle Down	
+# '^'	  Triangle Up	
+# '<'	  Triangle Left	
+# '>'	  Triangle Right	
+# '1'	  Tri Down	
+# '2'	  Tri Up	
+# '3'	  Tri Left	
+# '4'	  Tri Right	
+# '|'	  Vline	
+# '_'	  Hline

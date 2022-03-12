209ffeef4116d02651f843d0b2a798e313dcbdc9
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-12 22:06:11 +0530

    o added

--- 2.plotting_with_o.py
+++ 2.plotting_with_o.py
@@ -1,38 +1,8 @@
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.array([3,2,1])
-y=np.array([1,2,3])
-plt.plot(x,y, 'o')
+x=np.array([1,5,2,9])
+y=np.array([5,9,0,3])
+plt.plot(x,marker='o',ms='20',mec='b',mfc='m')
+plt.plot(y,marker='D',ms='10',mec='g',mfc='b')
 plt.show()
-
-
-#any marker shown below can be  used
-
-# Marker	Description
-# 'o'	    Circle	
-# '*'	    Star	
-# '.'	    Point	
-# ','	   Pixel	
-# 'x'   	X	
-# 'X'  	X (filled)	
-# '+' 	Plus	
-# 'P'	   Plus (filled)	
-# 's'	   Square	
-# 'D'	   Diamond	
-# 'd'	   Diamond (thin)	
-# 'p'	   Pentagon	
-# 'H' 	Hexagon	
-# 'h'	   Hexagon	
-# 'v'	  Triangle Down	
-# '^'	  Triangle Up	
-# '<'	  Triangle Left	
-# '>'	  Triangle Right	
-# '1'	  Tri Down	
-# '2'	  Tri Up	
-# '3'	  Tri Left	
-# '4'	  Tri Right	
-# '|'	  Vline	
-# '_'	  Hline

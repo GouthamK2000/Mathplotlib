0b5c86e8aa2dace2913a7f4954c7885d54deb684
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-14 06:50:37 +0530

    2 lines can be shown in the same plot using 4 inputs,under ply.plot

---
+++ 6.multilines.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+w=np.array([1,2,3,4])
+x=np.array([2,3,4,5])
+y=np.array([3,4,5,6])
+z=np.array([4,5,6,7])
+plt.plot(w,x,y,z)
+plt.show()
+
+#Here 2 lines gets printed

267248890d4c3491317544d18b36639bd9e3d630
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-19 07:49:13 +0530

    basic scater plot

---
+++ 11.scaterplot1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.array([1,4,6])
+y=np.array([4,7,3])
+
+plt.scatter(x,y)
+plt.plot()
+
+x=np.array([1,5,6])
+y=np.array([3,7,9])
+
+plt.scatter(x,y)
+
+plt.plot()
+
+#WE have plotted 2 things in the same graph. One plot can also be done.

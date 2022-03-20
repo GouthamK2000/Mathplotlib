0eae84d9e89840ab3e07370dbcdd7c38042982e2
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-20 20:43:59 +0530

    colors for each dot was added

--- 12.scatterplot1_colors.py
+++ 12.scatterplot1_colors.py
@@ -5,7 +5,7 @@ import numpy as np
 x=np.array([1,2,3])
 y=np.array([6,5,4])
 
-colors=np.array(["blue","green","red"])
+colors=np.array(["blue","green","red"]) #number of colors must be equal to the number of points that we plot
 
 plt.scatter(x,y,c=colors)
 plt.show()

587855fae97e73e6522e3ae982751efe1229c115
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-25 19:56:10 +0530

    alpha parameter,changes the visibility levels

---
+++ 16.fading_alpha.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.array([4,2,6])
+y=np.array([6,7,8])
+plt.scatter(x,y,color='magneta',alpha=0.5) #alpha values decides the transperancies of the markers
+plt.show()
+
+#alpha values lie between 0 and 1.

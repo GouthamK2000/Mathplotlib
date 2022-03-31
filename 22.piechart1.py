d4b4ef5ae64dd0d71817cdac2aadf1c68408742a
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-31 22:28:11 +0530

    piechart plotted

---
+++ 22.piechart1.py
@@ -0,0 +1,7 @@
+from tokenize import PlainToken
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.array([2,4,7,8])   # out of the 360 deg , the circle will be split in this ratio
+plt.pie(x)    #pie extension is used to plot a pie chart 
+plt.show()
